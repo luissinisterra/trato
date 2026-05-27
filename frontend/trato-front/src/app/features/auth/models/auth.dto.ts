b0b17307d192e57639f799e6919672e96db20938
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2026-05-27 14:05:10 -0500

    trato frontend

--- frontend/trato-front/src/app/features/auth/models/auth.dto.ts
+++
@@ -1,9 +0,0 @@
-export interface LoginDto {
-  email: string;
-  password?: string;
-}
-
-export interface RegisterDto {
-  email: string;
-  password?: string;
-}
--- frontend/trato-front/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: '/api' // To be configured for prod
-};
