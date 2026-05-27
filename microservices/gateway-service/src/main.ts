a28642f04ec200c9b9eef687e4587949c5ac2292
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2026-05-27 00:46:21 -0500

    chore: move gateway-service entry points (main.ts, app.module.ts)

--- microservices/gateway-service/src/main.ts
+++ microservices/gateway-service/src/main.ts
@@ -5,6 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
   app.use(cookieParser());
 
   const corsOrigin = process.env.CORS_ORIGIN || 'http://localhost:4200';
